e9ad986194d6a114d19eee6403554702917d0056
subhashree-farmint <subhashree@farmint.in>
2024-09-28 01:11:43 +0530

    tailwindcss

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,30 +6,31 @@ const Header = () => {
   let [btnName,setbtName] =useState("Login");
   const onlineStatus = useOnlineStatus();
     return (
-      <div className="header">
+      <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
         <div className="logo-container">
           <img
-            className="logo"
+            className="w-20"
             src={LOGO_URL}
           />
         </div>
-        <div className="nav-items">
-          <ul>
-          <li>Online Status: {onlineStatus ? "✅" : "🔴"}
+        <div className="flex items-center">
+          <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}
 
           </li>
-            <li><Link to="/">
-            Home</Link></li>
-            <li>
+            <li className="px-4"><Link to="/">
+            Home</Link>
+            </li>
+            <li className="px-4">
               <Link to="/about">About Us</Link>
             </li>
-            <li>
+            <li className="px-4">
               <Link to="/contact"> Contact Us  </Link>
               </li>
-              <li>
+              <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-             <li>Cart</li>
+             <li className="px-4">Cart</li>
             <button className="login" 
             onClick={()=> {
                 btnName === "Login"
